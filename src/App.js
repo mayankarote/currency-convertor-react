17086cb7eaa36eee3d5bc55eaf90fd10a02764c8
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-24 11:12:14 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+const BASE_URL =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=ae687192fbfa7959f17c362e52fb1d29&format=1";
 
 function App() {
+  const [CurrencyOptions, setCurrencyOptions] = useState([]);
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
+  }, []);
   return (
-    <div>
-      <h1>Hellow World</h1>
+    <div className="app">
+      <>
+        <h1>Convertor</h1>
+        <CurrencyRow />
+        <div className="equals">=</div>
+        <CurrencyRow />
+      </>
     </div>
   );
 }
